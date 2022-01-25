0bd626889a1ed45527f932f4ecc2d495d5aede9b
forestpax <forestmch2o@gmail.com>
2022-01-25 19:06:45 +0900

    lesson 4

--- mamazon/migrations/0001_initial.py
+++ mamazon/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Prodact',
+            name='Product',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=150)),
--- mamazon/models.py
+++ mamazon/models.py
@@ -3,8 +3,12 @@ from django.db import models
 # Create your models here.
 
 
-class Prodact(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=150)
     price = models.IntegerField()
     description = models.TextField()
     thumbnail = models.ImageField(upload_to='thumbnails/')
+
+    def __str__(self):
+        return self.name
+    
