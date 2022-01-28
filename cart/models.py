8f53a2990e87c1ab3903090e71751ac210d0350b
forestpax <forestmch2o@gmail.com>
2022-01-28 22:26:00 +0900

    lesson 08

--- cart/models.py
+++ cart/models.py
@@ -7,9 +7,8 @@ User = settings.AUTH_USER_MODEL
 # Create your models here.
 class Cart(models.Model):
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
-    products = models.ManyToManyField()
+    products = models.ManyToManyField(Product, blank=True)
     total = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-
 
